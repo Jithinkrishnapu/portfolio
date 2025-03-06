571ff37db128d04d9574cd4cb6e2bb292ceb0006
jithin krishna <jithinkrishna@jithins-MacBook-Air.local>
2025-03-06 16:23:05 +0530

    ui modifications

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ import Portfolio from './pages/portfolio';
 export const revalidate = 100
 export default function Home() {
   return (
-    <div className='px-2 h-[100vh] flex justify-center items-center bg-white' >
+    <div className='px-2 h-[100vh] flex justify-center items-center' >
       <Head>
-        <title>Portfolio</title>
-        <meta name="description" content="A simple flip card example using react-card-flip in a Next.js project" />
+        <title>Jithin Krishna</title>
+         <meta name="description" content="Explore my portfolio showcasing projects and skills in web and mobile development."/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <Portfolio />
